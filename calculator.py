78e991e9bbb9096cb60f2d87a5334705a294e088
Maxim Terentev <terentevmaxim1@gmail.com>
2023-03-07 18:27:54 +0300

    now tuples sorts in calculator

--- calculator.py
+++ calculator.py
@@ -3,12 +3,13 @@ from collections import defaultdict
 
 class Calculator:
     @staticmethod
-    def calculate(lst: list[dict[str, float]]) -> dict[str, list[float]]:
+    def calculate(lst: list[dict[str, float]]) -> list[tuple[str, list[float]]]:
         result = defaultdict(list)
+
         for el in lst:
             for k, v in el.items():
                 result[k].append(v)
-        return result
 
-
-
+        tuples = sorted(result.items(), key=lambda element: sum(element[1]), reverse=True)
+        print(tuples)
+        return tuples
